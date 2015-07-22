7a2441dab2a81c717cf56b3a5f9ab78af8ada92b
Roman Kondratev <romankondratev.ru@gmail.com>
2015-07-22 23:56:20 +0300

    Update.

---
+++ 1.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * 1.php
+ *
+ * @author      Roman Kondratev <romankondratev.ru@gmail.com>
+ * @copyright   Copyright (c) 2015, Roman Kondratev
+ * @link        http://romankondratev.ru
+ * @version     201507230056
+ */
+
